c0b74e8cc3b2bb8a4d0f699082d32e8b68962c82
gossi <github@gos.si>
2016-06-17 15:08:50 +0200

    Make list methods available to Set

--- src/Set.php
+++ src/Set.php
@@ -5,23 +5,23 @@ use \Iterator;
 
 /**
  * Represents a Set
- * 
+ *
  * @author Thomas Gossmann
  */
-class Set extends AbstractCollection {
-	
+class Set extends AbstractList {
+
 	/**
 	 * Creates a new Set
-	 * 
+	 *
 	 * @param array|Iterator $collection
 	 */
 	public function __construct($collection = []) {
 		$this->addAll($collection);
 	}
-	
+
 	/**
 	 * Adds an element to that set
-	 * 
+	 *
 	 * @param mixed $element
 	 * @return $this
 	 */
@@ -29,13 +29,13 @@ class Set extends AbstractCollection {
 		if (!in_array($element, $this->collection)) {
 			$this->collection[$this->size()] = $element;
 		}
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * Adds all elements to the set
-	 * 
+	 *
 	 * @param array|Iterator $collection
 	 * @return $this
 	 */
@@ -43,13 +43,13 @@ class Set extends AbstractCollection {
 		foreach ($collection as $element) {
 			$this->add($element);
 		}
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * Removes an element from the set
-	 * 
+	 *
 	 * @param mixed $element
 	 * @return $this
 	 */
@@ -61,7 +61,7 @@ class Set extends AbstractCollection {
 
 		return $this;
 	}
-	
+
 	/**
 	 * Removes all elements from the set
 	 *
