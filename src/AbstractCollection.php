f4acafdfa412a4463ea288eac73c91c175b102b3
gossi <github@gos.si>
2016-06-17 14:36:33 +0200

    No more deep toArray()

--- src/AbstractCollection.php
+++ src/AbstractCollection.php
@@ -4,68 +4,69 @@ namespace phootwork\collection;
 use phootwork\lang\Comparator;
 
 /**
- * AbstractCollection providing implemention for the Collection interface. 
- * 
+ * AbstractCollection providing implemention for the Collection interface.
+ *
  * @author Thomas Gossmann
  */
 abstract class AbstractCollection implements Collection {
-	
+
 	protected $collection = [];
-	
+
 	public function contains($element) {
 		return in_array($element, $this->collection, true);
 	}
-	
+
 	public function size() {
 		return count($this->collection);
 	}
-	
+
 	public function isEmpty() {
 		return count($this->collection) == 0;
 	}
-	
+
 	public function clear() {
 		$this->collection = [];
 	}
-	
+
 	public function toArray() {
-		return array_map(function ($v) {
-			if (is_object($v) && method_exists($v, 'toArray')) {
-				return $v->toArray();
-			}
-			return $v;
-		}, $this->collection);
+		return $this->collection;
+// 		return array_map(function ($v) {
+// 			if (is_object($v) && method_exists($v, 'toArray')) {
+// 				return $v->toArray();
+// 			}
+// 			return $v;
+// 		}, $this->collection);
 	}
-	
+
 	/**
 	 * Applies the callback to the elements
-	 * 
+	 *
 	 * @param callable $callback the applied callback function
 	 * @return static
 	 */
 	public function map(callable $callback) {
 		return new static(array_map($callback, $this->collection));
 	}
-	
+
 	/**
 	 * Filters elements using a callback function
-	 * 
+	 *
 	 * @param callable $callback the filter function
 	 * @return static
 	 */
 	public function filter(callable $callback) {
 		return new static(array_filter($this->collection, $callback));
 	}
-	
+
 	/**
 	 * Searches the collection for query using the callback function on each element
-	 * 
+	 *
 	 * The callback function takes one or two parameters:
-	 * 
+	 *
 	 *     function ($element [, $query]) {}
-	 * 
+	 *
 	 * The callback must return a boolean
-	 * 
+	 *
 	 * @param mixed $query (optional)
 	 * @param callable $callback
 	 * @return boolean
@@ -87,16 +88,16 @@ abstract class AbstractCollection implements Collection {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Searches the collection with a given callback and returns the first element if found.
-	 * 
+	 *
 	 * The callback function takes one or two parameters:
-	 * 
+	 *
 	 *     function ($element [, $query]) {}
-	 * 
+	 *
 	 * The callback must return a boolean
-	 * 
+	 *
 	 * @param mixed $query OPTIONAL the provided query
 	 * @param callable $callback the callback function
 	 * @return mixed|null the found element or null if it hasn't been found
@@ -108,18 +109,18 @@ abstract class AbstractCollection implements Collection {
 			$query = func_get_arg(0);
 			$callback = func_get_arg(1);
 		}
-		
+
 		foreach ($this->collection as $element) {
 			$return = func_num_args() == 1 ? $callback($element) : $callback($element, $query);
-			
+
 			if ($return) {
 				return $element;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Searches the collection with a given callback and returns the last element if found.
 	 *
@@ -140,11 +141,11 @@ abstract class AbstractCollection implements Collection {
 			$query = func_get_arg(0);
 			$callback = func_get_arg(1);
 		}
-		
+
 		$reverse = array_reverse($this->collection, true);
 		foreach ($reverse as $element) {
 			$return = func_num_args() == 1 ? $callback($element) : $callback($element, $query);
-			
+
 			if ($return) {
 				return $element;
 			}
@@ -152,7 +153,7 @@ abstract class AbstractCollection implements Collection {
 
 		return null;
 	}
-	
+
 	/**
 	 * Searches the collection with a given callback and returns all matching elements.
 	 *
@@ -177,18 +178,18 @@ abstract class AbstractCollection implements Collection {
 		$collection = [];
 		foreach ($this->collection as $k => $element) {
 			$return = func_num_args() == 1 ? $callback($element) : $callback($element, $query);
-				
+
 			if ($return) {
 				$collection[$k] = $element;
 			}
 		}
-		
+
 		return new static($collection);
 	}
-	
+
 	/**
 	 * Internal sort function
-	 * 
+	 *
 	 * @param array $collection the collection on which is operated on
 	 * @param Comparator|callable $cmp the compare function
 	 * @param callable $usort the sort function for user passed $cmd
@@ -212,35 +213,35 @@ abstract class AbstractCollection implements Collection {
 	public function rewind() {
 		return reset($this->collection);
 	}
-	
+
 	/**
 	 * @internal
 	 */
 	public function current() {
 		return current($this->collection);
 	}
-	
+
 	/**
 	 * @internal
 	 */
 	public function key() {
 		return key($this->collection);
 	}
-	
+
 	/**
 	 * @internal
 	 */
 	public function next() {
 		return next($this->collection);
 	}
-	
+
 	/**
 	 * @internal
 	 */
 	public function valid() {
 		return key($this->collection) !== null;
 	}
-	
+
 	/**
 	 * @internal
 	 */
